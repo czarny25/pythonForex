54a4455f7e28a87cf178685ca0972d9b2e843b94
Marty <Marty@DESKTOP-N1M9EJ9>
2020-04-13 08:58:46 +0100

    update 23

--- PythonForex/intro.py
+++ PythonForex/intro.py
@@ -1,7 +1,59 @@
-'''
-Created on 10 Apr 2020
 
-@author: Marty
-'''
-# this is intro to this project
 
+
+import numpy as np # numpy is external library and need to be imported
+
+
+# simple list 
+list = [1,2,3]
+
+# variable of numpy array type take list as argument
+x = np.array(list)
+
+print(type(x))
+print(x)
+
+# this is how numpy array turn multidementional array into matrix
+myMatrix = [[1,2,3],[3,4,6],[3,6,9]]
+print(np.array(myMatrix))
+
+myMatrix2D = [[[1,2,3],[3,4,6],[3,6,9]],[[1,2,3],[3,4,6],[3,6,9]],[[1,2,3],[3,4,6],[3,6,9]],[[1,2,3],[3,4,6],[3,6,9]]]
+print(np.array(myMatrix2D))
+
+# this method create list of specified range
+print(np.arange(0,5))
+
+print(np.arange(0,15,4))
+
+
+# this method specify list of zeros 
+print(np.zeros(4))
+
+# this method specify matrix of zeros 
+print( np.zeros((4,3)))
+
+
+# this method gives back list of ones 
+print(np.ones(4))
+
+# this method gives back matrix of ones
+print( np.ones((4,3)))
+
+# this is intresting method 
+# gives back a specified range of numbers between two numbers  
+print(np.linspace(3,4,7))
+
+# this method gives identyty matrix of ones
+print( np.eye(3))
+
+
+# this method gives back random num between given range
+#print( np.random.rand(1, 4))
+
+
+
+
+
+
+
+ 
